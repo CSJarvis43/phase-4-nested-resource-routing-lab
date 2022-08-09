a81f52844ee0696c0b2147f61023e428bd19521a
Charles Jarvis <csjarvis43@gmail.com>
2022-08-09 12:45:32 -0600

    commit tree c014f88bcabde19b6fe172e87381252050e569d6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,42 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(e)
+    render json: { error: "#{e}"}, status: :not_found
+  end
+
+
 
 end
